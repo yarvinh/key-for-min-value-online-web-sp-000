39b013af4625045a5ce9b80e8c2305f43e6aa4d6
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-03 14:53:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  by_num = name_hash.map {|name|
+          name
+      }
+      if by_num == []
+        nil
+      else
+      names = by_num[0][0]
+      counter = by_num[0][1]
+      by_num.map {|num|
+          if  num[1] < counter
+              names = num[0]
+          end
+      }
+      names
+    end
 end

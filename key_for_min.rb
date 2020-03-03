9e4b1baf9231b01eb0c7e64efb2967b4942ddf4d
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-03 14:59:58 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  key_and_value = name_hash.map {|value|
+          value
+      }
+      if key_and_value == []
+        nil
+      else
+      name = key_and_value[0][0]
+      counter = key_and_value[0][1]
+      key_and_value.map {|num|
+          if  num[1] < counter
+              name = num[0]
+          end
+      }
+      name
+    end
 end
